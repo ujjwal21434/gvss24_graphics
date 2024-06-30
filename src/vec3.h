48f720a7ede7a018146d87a8f83599735993a100
rupesh <rupesh@Rupesh-PC>
2024-06-30 15:01:49 +0530

    added basic code

--- src/vec3.h
+++ src/vec3.h
@@ -105,25 +105,4 @@ inline vec3 unit_vector(const vec3& v) {
     return v / v.length();
 }
 
-
-inline vec3 random_in_unit_sphere() {
-    while (true) {
-        auto p = vec3::random(-1,1);
-        if (p.length_squared() < 1)
-            return p;
-    }
-}
-
-inline vec3 random_unit_vector() {
-    return unit_vector(random_in_unit_sphere());
-}
-
-inline vec3 random_on_hemisphere(const vec3& normal) {
-    vec3 on_unit_sphere = random_unit_vector();
-    if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
-        return on_unit_sphere;
-    else
-        return -on_unit_sphere;
-}
-
 #endif
