39410d1c1273c985b4fb4dd68d0ef1fb2d6d95bf
rupesh <rupesh@Rupesh-PC>
2024-07-02 15:01:35 +0530

    added viewport code

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # GVSS Assignment
 
 Make sure that [GLEW](https://glew.sourceforge.net/), [glm](https://github.com/g-truc/glm), and [SDL2](https://www.libsdl.org/) are installed. Ideally, these should be installed by your package manager rather than manually (at least, if you are on Linux or Mac).
@@ -10,4 +11,6 @@ cd build
 cmake ..
 make
 ```
+Gists: 
+https://gist.github.com/rupeshkmr/29b0c7e0a66ab99e8e078346ad56fb3f - Viewport
 
--- raytracing/basic_image.cpp
+++ raytracing/basic_image.cpp
@@ -13,6 +13,26 @@ int main() {
     // Calculate the image height, and ensure that it's at least 1.
     int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+    
+    // Camera
+    float focal_length = 1.0;
+    float viewport_height = 2.0;
+    float viewport_width = viewport_height * (double(image_width) / image_height);
+    point3 camera_center = point3(0,0,0);
+
+    // Compute Vu and Vv
+    vec3 viewport_u = vec3(viewport_width, 0, 0);
+    vec3 viewport_v = vec3(0, -viewport_height, 0);
+    
+    // Compute du and dv
+    vec3 pixel_delta_u = viewport_u / image_width;
+    vec3 pixel_delta_v = viewport_v / image_height;
+
+    // Calculate the location of upper left pixel
+    point3 viewport_upper_left = camera_center - vec3(0,0,focal_length) - viewport_u / 2 - viewport_v /2;
+    point3 pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+
 
     // Render
 
